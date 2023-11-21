e7d8454f39c92d777e6ecbaed058abef603b5ede
Bright Jonathan <brightjonathan000@gmail.com>
2023-11-21 09:35:14 +0100

    updated the firebase config file

--- client/src/Firebase/FirebaseConfig.js
+++ client/src/Firebase/FirebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-IQLDEJyo4wMBqrd9eLS8mMiFJBpnx4c",
-  authDomain: "e-ventory-a143d.firebaseapp.com",
-  projectId: "e-ventory-a143d",
-  storageBucket: "e-ventory-a143d.appspot.com",
-  messagingSenderId: "420759612641",
-  appId: "1:420759612641:web:6003585a83db7c31e8dba0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
+
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
